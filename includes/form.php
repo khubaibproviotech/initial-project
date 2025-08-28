98fd87905783386d031828b6cf61a1adc46e989c
khubaibproviotech <khubaibproviotech@gmail.com>
2025-08-29 02:05:18 +0500

    add form

---
+++ includes/form.php
@@ -0,0 +1,20 @@
+<div class="form-wrapper">
+    <form action="javascript:" class="leadform" method="POST" novalidate="novalidate">
+
+        <div class="form-group">
+            <input name="name" type="text" placeholder="Your Name" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <input name="email" type="email" placeholder="Your Email" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <input name="phone" type="text" placeholder="Phone Number" class="form-control">
+        </div>
+        
+        <button type="submit" class="btn btn-primary form-btn center">
+            <i class="btn-curve"></i><span class="btn-title">Submit</span></button>
+
+    </form>
+</div>
