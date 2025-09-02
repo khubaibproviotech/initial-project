e08d6c3c1527f1c77f758bbde9fa3903efe292ad
khubaibproviotech <khubaibproviotech@gmail.com>
2025-09-02 23:59:46 +0500

    add footer with copyright and disclaimer information

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+
+    <div>
+        Amazon Classic Publishers | © Copyright 2024
+        Disclaimer: Amazon Classic Publishers operates as a fully independent entity with no affiliation to Amazon.com
+        or any of its subsidiaries. We are not a part of Amazon, nor are we an official partner or affiliate. Our
+        mission is to support independent authors with their publishing and marketing needs. We are committed to ethical
+        business practices and operate in full compliance with all applicable U.S. Federal and State regulations.
+    </div>
+</footer>
